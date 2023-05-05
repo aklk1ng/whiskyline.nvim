5334c70da54996489ea01dfc238c59f274f73cf2
aklk1ng <2628084745@qq.com>
2023-05-05 15:51:52 +0800

    update the provider.lua

--- lua/whiskyline/provider.lua
+++ lua/whiskyline/provider.lua
@@ -142,11 +142,13 @@ function pd.filesize()
     local suffix = { 'b', 'k', 'M', 'G', 'T', 'P', 'E' }
     local fsize = vim.fn.getfsize(vim.api.nvim_buf_get_name(0))
     fsize = (fsize < 0 and 0) or fsize
-    if fsize < 1024 then
-      return fsize .. suffix[1]
+    local i = 1
+    while fsize > 1024 and i < #suffix do
+      fsize = fsize / 1024
+      i = i + 1
     end
-    local i = math.floor((math.log(fsize) / math.log(1024)))
-    return string.format('%.2g%s', fsize / math.pow(1024, i), suffix[i + 1])
+    local format = i == 1 and '%d%s' or '%.1f%s'
+    return string.format(format, fsize, suffix[i])
   end
   local result = {
     stl = get_size,
@@ -180,10 +182,7 @@ function pd.search_count()
     if vim.v.hlsearch == 0 then
       return ''
     end
-    local search = vim.fn.searchcount()
-    if next(search) == nil then
-      return ''
-    end
+    local search = vim.fn.searchcount({ maxcount = 999, timeout = 100 })
 
     return string.format('[%d/%d]', search.current, math.min(search.total, search.maxcount))
   end
